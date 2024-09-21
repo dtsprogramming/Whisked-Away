8dfd0a97d7b3631acc93c782285c2c0dbf2f40e3
Dan <sidato83@gmail.com>
2024-09-21 02:19:36 -0700

    Completed stove scene and added fire

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,27 +5,27 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
 
-    private int flipDir = 1;
+    private float flipDir = 1.2f;
 
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
 
-        if (Input.GetMouseButtonDown(0)) {
+        if (Input.GetMouseButtonDown(0)) 
+        {
             flipDir *= -1;
             FlipPlayer(flipDir);
+        }
     }
-}
 
-    private void FlipPlayer(int direction)
+    private void FlipPlayer(float direction)
     {
-        transform.localScale = new Vector3(direction, 1, 1);
+        transform.localScale = new Vector3(direction, 1.2f, 1.2f);
 
         moveSpeed *= -1;
     }
