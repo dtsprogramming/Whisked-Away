bf4259655f50bb286e4aa306181963def0116a32
Dan <sidato83@gmail.com>
2024-09-21 00:45:09 -0700

    Added player motion and scene animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,8 +1,12 @@
+using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float moveSpeed = 5f;
+
+    private int flipDir = 1;
+
     void Start()
     {
         
@@ -11,6 +15,18 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+
+        if (Input.GetMouseButtonDown(0)) {
+            flipDir *= -1;
+            FlipPlayer(flipDir);
+    }
+}
+
+    private void FlipPlayer(int direction)
+    {
+        transform.localScale = new Vector3(direction, 1, 1);
+
+        moveSpeed *= -1;
     }
 }
